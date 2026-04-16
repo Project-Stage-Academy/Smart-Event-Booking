655f586951b41ca98570fe2b2232f5887a604ff1
Ivan Lutsenko <108870263+eevatsen@users.noreply.github.com>
2026-04-17 02:27:43 +0300

    fix: loginDto

---
+++ src/SmartEventBooking.Application/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartEventBooking.Application.DTOs.Auth
+{
+    public class LoginDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
