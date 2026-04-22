d2822d90baca22ace9d99ec9fdce83c34d138e8b
Mykhailo B <mykhailo.bilousov.shi.2024@lpnu.ua>
2026-04-22 09:05:26 +0300

    feat(auth): add validation to login dto

--- src/api/SmartEventBooking.Application/DTOs/Auth/LoginDto.cs
+++ src/api/SmartEventBooking.Application/DTOs/Auth/LoginDto.cs
@@ -5,11 +5,14 @@ namespace SmartEventBooking.Application.DTOs.Auth
     public class LoginDto
     {
         [Required]
+        [EmailAddress]
         public required string Email { get; set; }
 
         [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public required string Password { get; set; }
-        
+
         [Required]
         public bool RememberMe { get; set; }
     }
