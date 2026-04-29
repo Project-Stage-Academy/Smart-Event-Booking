763f9e290e292c871eb71bcc4e81a54252009e1c
viktorpekhun <129255792+viktorpekhun@users.noreply.github.com>
2026-04-29 17:49:01 +0300

    minor fix

--- tests/SmartEventBooking.Infrastructure.UnitTests/Identity/AuthServiceTests.cs
+++ tests/SmartEventBooking.Infrastructure.UnitTests/Identity/AuthServiceTests.cs
@@ -269,7 +269,7 @@ public class AuthServiceTests
         var result = await _authService.LoginAsync(dto);
 
         result.Succeeded.Should().BeFalse();
-        result.Errors.Should().ContainSingle().Which.Should().Be("Login failed.");
+        result.Errors.Should().ContainSingle().Which.Should().Be("Login failed: Please verify your email (should never happen for now).");
     }
 
     [Fact]
@@ -298,7 +298,7 @@ public class AuthServiceTests
         var result = await _authService.LoginAsync(dto);
 
         result.Succeeded.Should().BeFalse();
-        result.Errors.Should().ContainSingle().Which.Should().Be("Login failed: Please verify your email (should never happen for now).");
+        result.Errors.Should().ContainSingle().Which.Should().Be("Login failed.");
     }
 
     [Theory]
