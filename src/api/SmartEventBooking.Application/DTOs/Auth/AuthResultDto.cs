0664268d96e8e6ea4ca82916b9ceb2d3ffc771d3
Ivan Lutsenko <108870263+eevatsen@users.noreply.github.com>
2026-04-30 14:35:36 +0300

    fix AuthResultDto.cs

--- src/api/SmartEventBooking.Application/DTOs/Auth/AuthResultDto.cs
+++ src/api/SmartEventBooking.Application/DTOs/Auth/AuthResultDto.cs
@@ -6,6 +6,5 @@ namespace SmartEventBooking.Application.DTOs.Auth
         public bool Succeeded { get; set; }
         public IEnumerable<string> Roles { get; set; } = Array.Empty<string>();
         public IEnumerable<string> Errors { get; set; } = Array.Empty<string>();
-        public IEnumerable<string> Roles { get; set; } = Array.Empty<string>();
     }
 }
